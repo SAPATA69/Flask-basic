cacbbeb6b63ad5de495270b58ac536d851dbee7c
SAPATA69 <crca4577@gmail.com>
2025-12-09 10:49:48 +0700

    add

--- Sever.py
+++ Sever.py
@@ -1,10 +1,52 @@
 from flask import Flask
+import uuid
 
 app = Flask(__name__)
 
+
 @app.route('/name')
 def index():
-    return 'Welcome to the Sever application!'
+    
+    return f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to the Sever application!</h1>
+</body>
+</html>
+"""
+
+@app.route('/user/<username>')
+def user(username):
+    
+    return f'<h1>my name is, {username}!!</h1>'
+
+@app.route('/calculate/addition/<int:a>/<int:b>')
+def addition(a, b):
+    result = a + b
+    return f'<h1>{a} + {b} = {a+b}</h1>'
+
+@app.route('/calculate/addition/<int:a>/<int:b>')
+def addition(a, b):
+    result = a - b
+    return f'<h1>{a} - {b} = {a-b}</h1>'
+
+@app.route('/calculate/addition/<int:a>/<int:b>')
+def addition(a, b):
+    result = a * b
+    return f'<h1>{a} * {b} = {a*b}</h1>'
+
+
+@app.route('/secretkey/<uuid:sk>')
+def my_secretkey(sk):
+    return f'<h1>your secret key is {sk}</h1>'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
